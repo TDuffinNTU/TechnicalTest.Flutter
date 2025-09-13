222548d344888a71563a3c06fa9ecf708d6766e8
Tom Duffin <thomaslukeduffin@hotmail.co.uk>
2025-09-13 15:30:39 +0100

    Docs and removing the mocks i forgot to add to gitignore oops

--- lib/repository/common/api_client.dart
+++ lib/repository/common/api_client.dart
@@ -4,10 +4,13 @@ final Dio _dio = Dio(
   BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com'),
 );
 
-/// Common API methods, wrapped for mocking.
+/// Common API methods, implemented with [Dio].
+/// Wrapped for mocking in mockito.
 class ApiClient {
-  Future<List<dynamic>> getRequest(CancelToken? ct, String path) async {
+  Future<List<dynamic>> getRequest(String path, CancelToken? ct) async {
     final response = await _dio.get<List<dynamic>>(path);
     return response.data!;
   }
+
+  // Imagine there are post/put/patch etc. here!
 }
--- lib/repository/common/api_controller.dart
+++ lib/repository/common/api_controller.dart
@@ -3,14 +3,16 @@ import 'package:flutter_tech_task/repository/comments/comment.dart';
 import 'package:flutter_tech_task/repository/common/api_client.dart';
 import 'package:flutter_tech_task/repository/common/response_parsers.dart';
 
+/// JsonPlaceholder API Controller.
 class ApiController {
   final ApiClient client = ApiClient();
 
+  /// Gets a list of all the comments for the post under [postId]
   Future<List<Comment>> getComments(
     CancelToken? ct, {
     required String postId,
   }) async {
-    final data = await client.getRequest(ct, '/post/$postId/comments');
+    final data = await client.getRequest('/post/$postId/comments', ct);
 
     return parseListResponse<Comment>(data, (json) => Comment.fromJson(json));
   }
