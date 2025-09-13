4945df2065c968c433dcb83cc264af74b3670a15
Tom Duffin <thomaslukeduffin@hotmail.co.uk>
2025-09-13 12:32:46 +0100

    add analyzer and other tasks for running on other machines.

---
+++ scripts/analyze.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Runs the analyzers for dart and riverpod.
+
+dart analyze
+dart run custom_lint
---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Runs build_runner to generate dart code.
+
+dart run build_runner build --delete-conflicting-outputs
