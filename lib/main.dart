8873dd0afd04b50487ffe168024d86688cbea0e2
Tom Duffin <thomaslukeduffin@hotmail.co.uk>
2025-09-13 12:32:30 +0100

    add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tech_task/presentation/home/screens/home_screen.dart';
 import 'package:flutter_tech_task/presentation/posts/screens/post_details_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: 'home/',
       routes: <String, WidgetBuilder>{
         'home/': (context) => const ListPage(),
