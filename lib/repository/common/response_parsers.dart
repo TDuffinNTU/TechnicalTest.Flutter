82579a14791d05df906857a1028d1d8129216bd1
Tom Duffin <thomaslukeduffin@hotmail.co.uk>
2025-09-13 21:15:21 +0100

    doc strings and modified tasks

--- lib/repository/common/response_parsers.dart
+++ lib/repository/common/response_parsers.dart
@@ -1,10 +1,11 @@
-/// Parse the response of an API Request (List of dynamic) into an object's
-/// fromJson() method.
+/// Parse the response of an API request to a list of Objects using their
+/// [fromJson] methods.
 List<T> parseListResponse<T>(
   Map<String, dynamic> data,
   T Function(Map<String, dynamic>) fromJson,
 ) => (data['data'] as List<dynamic>).map((e) => fromJson(e)).toList();
 
+/// Parse the response of an API request to an Object's [fromJson] method.
 T parseSingleResponse<T>(
   Map<String, dynamic> data,
   T Function(Map<String, dynamic>) fromJson,
