c1f7cce26d8f551e1295ad41aac26609dc9bb02e
Tom Duffin <thomaslukeduffin@hotmail.co.uk>
2025-09-14 13:43:14 +0100

    fix API weirdness

--- lib/repository/data_sources/database/api_client.dart
+++ lib/repository/data_sources/database/api_client.dart
@@ -1,14 +1,20 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 final Dio _dio = Dio(
-  BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com'),
+  BaseOptions(
+    baseUrl: 'https://jsonplaceholder.typicode.com',
+    contentType: ContentType.json.toString(),
+    responseType: ResponseType.json,
+  ),
 );
 
 /// Common API methods, implemented with [Dio].
 /// Wrapped for mocking in mockito.
 class ApiClient {
   Future<Map<String, dynamic>> getRequest(String path, CancelToken? ct) async {
-    final response = await _dio.get(path);
+    final Response response = await _dio.get(path);
     return {'data': response.data!};
   }
 
